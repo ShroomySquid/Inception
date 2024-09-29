2bd2516d86169107cd3bc7962d8da3ab7ed6d826
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-09-29 17:54:51 -0400

    changed to debian bullseye, mariadb seems to work fine now but wordpress still doesn't have access to db and have problem running php file

--- srcs/mariadb/tools/entrypoint.sh
+++ srcs/mariadb/tools/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-sudo service mysql start 
+mkdir -p /var/run/mysql
+chown -R mysql:mysql /var/lib/mysql
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
+mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db >> /dev/null
+
+echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < db1.sql
+mysqld --user=mysql < db1.sql
 
-echo "running mysqld..."
+rm -f db1.sql
 
-mysqld_safe --sjip-grant-tables
-
-echo "mysqld done"
+exec mysqld_safe
--- srcs/wordpress/tools/entrypoint.sh
+++ srcs/wordpress/tools/entrypoint.sh
@@ -14,23 +14,27 @@ wp core download --allow-root
 
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-mv /wp-config.php /var/www/html/wp-config.php
+sed -i -r "s/db1/$db_name/"   wp-config.php
+sed -i -r "s/user/$db_user/"  wp-config.php
+sed -i -r "s/pwd/$db_pwd/"    wp-config.php
 
+#until mysql -u"$db_user" -p"$db_pwd" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+#    echo "Waiting for database connection..."
+#	sleep 5
+#done
 
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
+sleep 10
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 wp theme install astra --activate --allow-root
 
 wp plugin update --all --allow-root
- 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir /run/php
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
