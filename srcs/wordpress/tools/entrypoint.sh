dbf5373c31cea073e9bac1d5f2f2ee798dc77212
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-10-12 11:57:14 -0400

    not much

--- srcs/wordpress/tools/entrypoint.sh
+++ srcs/wordpress/tools/entrypoint.sh
@@ -24,7 +24,7 @@ fi
 if [ ! -f /var/www/html/wp-config.php ]; then
   wp core download --allow-root --path=/var/www/html
   wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
-  wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_name="${WP_ADMIN}" --admin_password="${WP_PASSWORD}" --admin_email="${WP_EMAIL}" --skip-email
+  wp core install --allow-root --home="${DOMAIN_NAME}" --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_name="${WP_ADMIN}" --admin_password="${WP_PASSWORD}" --admin_email="${WP_EMAIL}" --skip-email
   wp user create --allow-root "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASSWORD}" --role=author
   /usr/sbin/php-fpm7.4 -F
 fi
